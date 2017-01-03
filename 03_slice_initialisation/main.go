b758cff2edbbb1a63d04a53afcb8869a64e1be4a
Mykola Skorenkyi <mykola.skorenky@gmail.com>
2017-01-03 15:43:36 +0200

    Slice initialisation

---
+++ 03_slice_initialisation/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare a []string and initalise it with the names of three people in this room
+	names := []string{"foo", "bar", "xyz"}
+	fmt.Println(names)
+}
